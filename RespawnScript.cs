df034c84f22a22ec89548956261783227c890ba5
AnnekeWelbedacht <47172320+AnnekeWelbedacht@users.noreply.github.com>
2019-02-13 08:56:00 +0100

    RespawnScript

---
+++ RespawnScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform RespawnPoint1;
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = RespawnPoint1.transform.position;
+    }
+}
